rust-crypto-labs/rust-dense-bitset

commit 1990e7535abc2131cd73ae019aecc06b2a03331a

Feat(vec64impl): insert() and insert_u64() can now specify a length parameter

src/vec64impl.rs
@@ -143,49 +143,61 @@ impl DenseBitSetExtended {
         }
     }
 
-    /// Inserts a `DenseBitSet` at the given `position`, which extends and overwrites the current one
+    /// Inserts the first `length` bits of `other` at the given `position` in the current structure
     pub fn insert(&mut self, other: &Self, position: usize) {
         let l = other.state.len();
         let size_before_insertion = self.size;
-        for i in 0..l {
-            self.insert_u64(other.state[i], position + i * 64);
+        if other.size % 64 == 0 {
+            for i in 0..l {
+                self.insert_u64(other.state[i], position + i * 64, 64);
+            }
         }
-
-        // By default, insert_u64 sets the size to the next multiple of 64
-        // but we can truncate more precisely here
-        self.size = max(size_before_insertion, position + l * 64);
-
-        println!("vecsize: {}", self.state.len());
+        else {
+            for i in 0..l-1 {
+                self.insert_u64(other.state[i], position + i * 64, 64);
+            }
+            self.insert_u64(other.state[l-1], position + (l-1) * 64, other.size % 64);
+        }
+    
+        self.size = max(size_before_insertion, position + other.size);
     }
 
-    /// Inserts an integer as a bitset at the given `position`
-    pub fn insert_u64(&mut self, i: u64, position: usize) {
+    /// Inserts a `length`-bit integer as a bitset at the given `position`
+    pub fn insert_u64(&mut self, value: u64, position: usize, length: usize) {
         let idx = position >> 6;
         let offset = position % 64;
 
         // First, resize the bitset if necessary
-        if 1 + idx > self.state.len() {
+        if 1 + ((position + length)>>6)  > self.state.len()  {
             // We need to extend the bitset to accomodate this insertion
-            let num_seg = 1 + idx - self.state.len();
+            let num_seg = 1 + ((position + length)>>6) - self.state.len();
 
-            for _ in 0..=num_seg {
+            for _ in 0..num_seg {
                 self.state.push(0);
             }
         }
-        self.size = max(self.size, position + 64);
+        self.size = max(self.size, position + length);
 
         // Second, perform the actual insertion
-        if offset == 0 {
-            // Easy case: the u64 matches an element
-            self.state[idx] = i;
+        if offset == 0 && length == 64 {
+            // Easiest case
+            self.state[idx] = value;
+        }
+        else if offset + length < 64 {
+            // Easy case: inserting fewer than 64 bits in an u64
+            let mut u = u64::max_value();
+            u ^= ((1 << length) - 1) << offset;
+            self.state[idx] &= u;
+            self.state[idx] |= value << position;
         } else {
-            // We need to split `i` in twain, zero the appropriate bits in the
-            // to segments, and perform the insertion
+            // Not so easy case: we need to split `value` in twain, zero the appropriate bits in the
+            // two segments, and perform the insertion
 
-            let lsb = (i & ((1 << (64 - offset)) - 1)) << offset;
-            let msb = i >> (64 - offset);
+            let lsb = (value & ((1 << (64 - offset)) - 1)) << offset;
             let mask_lsb = u64::max_value() >> (64 - offset);
-            let mask_msb = u64::max_value() << offset;
+
+            let msb = value >> (64 - offset);
+            let mask_msb = u64::max_value() << ((position + length) % 64);
 
             self.state[idx] = (self.state[idx] & mask_lsb) | lsb;
             self.state[idx + 1] = (self.state[idx + 1] & mask_msb) | msb;

src/lib.rs
@@ -582,7 +582,7 @@ mod tests {
     #[test]
     fn test_insert_u64_dbse() {
         let mut bs = DenseBitSetExtended::new();
-        bs.insert_u64(0b1011011101111, 50);
+        bs.insert_u64(0b1011011101111, 50, 64);
 
         assert_eq!(bs.to_string(), "00000000000000000000000000000000000000000000000000000000000000000101101110111100000000000000000000000000000000000000000000000000");
     }

commit 284f63abfbd5821a048fbcc22f7181396f4c7b2a

Fix(vec64impl): Fixed a bug in insert_u64() introduced in commit 1990e75

src/vec64impl.rs
@@ -188,7 +188,7 @@ impl DenseBitSetExtended {
             let mut u = u64::max_value();
             u ^= ((1 << length) - 1) << offset;
             self.state[idx] &= u;
-            self.state[idx] |= value << position;
+            self.state[idx] |= value << offset;
         } else {
             // Not so easy case: we need to split `value` in twain, zero the appropriate bits in the
             // two segments, and perform the insertion
@@ -573,6 +573,7 @@ impl ShlAssign<usize> for DenseBitSetExtended {
 impl Shr<usize> for DenseBitSetExtended {
     type Output = Self;
     fn shr(self, rhs: usize) -> Self {
+
         if rhs >= self.size {
             Self {
                 state: vec![],
@@ -580,7 +581,7 @@ impl Shr<usize> for DenseBitSetExtended {
             }
         } else {
             let mut v = DenseBitSetExtended::with_capacity(self.size - rhs);
-
+            
             // Note: this may not be the most efficient implementation
             for i in 0..(self.size - rhs) {
                 let source = i + rhs;